47db58aaf9ea1aa53eaf3f0fd58f15b84d3829e2
LarsGJobloop <lars.gunnar@jobloop.no>
2024-05-03 11:49:09 +0200

    setup function for generating person cards

--- script.js
+++ script.js
@@ -1,1 +1,40 @@
-console.log('Hello!');
+function createPersonCard(person) {
+    console.log(person)
+    // Genere et html element
+    const articleElement = document.createElement("article")
+
+    // Lage alle elementenen
+    const firstName = document.createElement("p")
+    const age = document.createElement("p")
+    const profileLink = document.createElement("a")
+
+    // Legg til innholdet i elementene
+    articleElement.className = "person-card"
+    firstName.textContent = person.name
+    age.textContent = person.age
+    profileLink.textContent = "Profile"
+    profileLink.href = person.homePage
+
+    // Lagt de til kontaineren
+    articleElement.appendChild(firstName)
+    articleElement.appendChild(age)
+    articleElement.appendChild(profileLink)
+
+    return articleElement
+}
+
+// Hvilken data har vi
+const lars = {
+    name: "Lars G",
+    age: 34,
+    homePage: "www.github.com/larsg",
+}
+
+// Kall funksjonen med persons objekt vårt
+const larsElement = createPersonCard(lars)
+
+// Sette inn elementet i dokumentet
+// 1. Finn ut hvor vi skal sette elementet inn
+const container = document.getElementById("single-person")
+// 2. Sett inn det nye elemntet som et under element
+container.appendChild(larsElement)
