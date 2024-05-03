482f08a2a98aee7a495dea51738839f71b51c0e6
LarsGJobloop <lars.gunnar@jobloop.no>
2024-05-03 12:02:05 +0200

    add more persons to data list

--- data.js
+++ data.js
@@ -9,4 +9,14 @@ export const persons = [
         age: 19,
         homePage: "www.github.com/pers",
     },
+    {
+        name: "Pål",
+        age: 42,
+        homePage: "www.github.com/pål",
+    },
+    {
+        name: "Espen",
+        age: 17,
+        homePage: "www.github.com/espen",
+    },
 ]
